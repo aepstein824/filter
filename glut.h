0dce4e1d223a7143c6df4d27d905e66fae046d73
Aaron Epstein <Aaron Epstein>
2012-07-11 00:29:12 -0700

    Builds on windows. I'm a boss.

--- glut.h
+++ glut.h
@@ -1,5 +1,7 @@
 #ifdef __APPLE__
 #include <GLUT/glut.h>
+#elif _WIN32
+#include <GL/glut.h>
 #else
 #include <glut.h>
 #endif
--- premake4.lua
+++ premake4.lua
@@ -8,20 +8,25 @@ solution "Threebands"
   kind "WindowedApp"
   language "C"
   files { "*.h", "*.c" }
-  links {"jack",  "m"}
+  links {"m"}
  
   configuration "Debug"
    defines {"DEBUG"}
    flags {"Symbols"}
+
+  configuration "windows"
+   includedirs {"/c/Program Files (x86)/Jack/includes", "/c/mingw/include", "/c/mingw/msys/1.0/include", "freeglut/include"}
+   libdirs {"/c/Program Files (x86)/Jack/lib", "freeglut/lib"}
+   links {"libjack", "OpenGL32", "libfreeglut", "glu32"}
    
   configuration "linux"
    includedirs {"/usr/include", "/usr/local/include/GL", "/usr/include/GL"}
    libdirs {"/usr/local/lib"}
-   links {"pthread", "rt", "GL", "GLU", "glut"}
+   links {"pthread", "rt", "GL", "GLU", "glut", "jack"}
   
   configuration "macosx"
    includedirs {"/usr/include", "usr/local/include", "/usr/local/include/GL", "/usr/include/GL"}
-   links {"OpenGL.framework", "GLUT.framework"}
+   links {"OpenGL.framework", "GLUT.framework", "jack"}
 
   configuration "gmake"
    buildoptions {"-std=c99"}
